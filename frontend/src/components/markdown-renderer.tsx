a9134342f7897c27f94fc30b8edc51c7860cc655
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-05-09 22:36:59 +0800

    feat(markdown-renderer): add antd.Button external render

--- frontend/src/components/markdown-renderer.tsx
+++ frontend/src/components/markdown-renderer.tsx
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import { Button } from 'antd';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkHtml from 'remark-html';
@@ -9,6 +10,25 @@ const MarkdownRenderer = ({content} : any) =>{
 
     const themeCtx = useContext(ThemeContext);
 
+    const externalRender = {
+        // replace html button to antd.Button
+        button({node, antd_type, className, children, ...props}) {
+            return (
+                <Button 
+                    className={className} type={antd_type} 
+                    style={{padding: '0 10px'}}
+                    {...props}
+                >
+                    {children}
+                </Button>
+            )
+        },
+    }
+
+    const renderers = {
+        ...externalRender,
+    };
+
     return(
         <div style={{ wordWrap: 'break-word', overflowWrap: 'break-word'}} className={themeCtx.userTheme}>
         <ReactMarkdown
@@ -16,6 +36,7 @@ const MarkdownRenderer = ({content} : any) =>{
             remarkPlugins={[remarkGfm, remarkHtml]}
             rehypePlugins={[rehypeRaw]}
             skipHtml={false}
+            components={renderers}
         >
             {content}
         </ReactMarkdown>
